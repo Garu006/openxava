2236e3ad6d9250c587f1f0919e379c0e6f8cca98
Garu006 <garojas@uamv.edu.ni>
2025-11-03 18:00:31 -0600

    primer proyecto de openxava

---
+++ facturacion1/src/main/java/com/tuempresa/facturacion1/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package com.tuempresa.facturacion1.modelo;
+
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+
+    @Id
+    @Column(length = 6)
+    int numero;
+
+    @Column(length = 50)
+    @Required
+    String nombre;
+}
